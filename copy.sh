f5ef81bb9900ba89e0ab7e92c770cc403cb3a30b
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2023-09-10 20:03:26 +0700

    Config file and path fixed (#3)

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 curl https://rclone.org/install.sh | bash
-curl $CONFIG_URL> rclone.conf
-rclone copy $SOURCE_RC $DESTINATION_RC --config=rclone.conf
+echo -e "$RCLONE_CONFIG" > rclone.conf
+rclone copy "$SOURCE_RC" "$DESTINATION_RC" --config=rclone.conf
