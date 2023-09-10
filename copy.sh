f368ce8b6f73674060d275eda2401c9eb698e16c
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2023-09-10 20:02:34 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 curl https://rclone.org/install.sh | bash
 echo -e "$RCLONE_CONFIG" > rclone.conf
-rclone copy $SOURCE_RC $DESTINATION_RC --config=rclone.conf
+rclone copy "$SOURCE_RC" "$DESTINATION_RC" --config=rclone.conf
